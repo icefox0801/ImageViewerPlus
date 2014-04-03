d42d5a5c670ee7b21ffb1632365d6f3674ccdf10
icefox <icefox0801@hotmail.com>
2014-04-03 17:40:22 +0800

    adding thumb slide methodn

--- public/javascripts/imageViewerPlus.js
+++ public/javascripts/imageViewerPlus.js
@@ -3,17 +3,20 @@
  *
  **/
 (function($){
+
     /* ImageViewer Class Definition*/
     var ImageViewer = function (elem, album, options) {
 
         this.currentIndex = options.currentIndex;
-        this.maxIndex = album.length;
+        this.maxIndex = album.length - 1;
+        this.thumbNumber = Math.floor(options.width / ( options.thumbWidth + 5)); // 每页的缩略图数 
         this.album = album;
         this.$elem = $(elem);
         this.$wrapper = options.wrapper === false ? $('body') : $(options.wrapper);
         this.$img = null;
         this.$navi = null;
         this.$ul = null;
+        this.thumbOffset = 0;
         this.list = [];
         this.transitionSetting = options.transitionSetting;
         this.init(options);
@@ -59,9 +62,8 @@
                 'max-width': width,
                 'max-height': height
             });
-            // $img[0].src = _t.album[_t.currentIndex];
 
-            $wrapper.append($prev).append($next).append($content);
+            $wrapper.append($content).append($prev).append($next);
 
             // 事件绑定
             $prev.on('click', function (event) {
@@ -91,6 +93,7 @@
 
             var _t = this,
                 $navi = $('<div/>'),
+                $naviWrapper = $('<div/>'),
                 $naviPrev = $('<div/>'),
                 $naviNext = $('<div/>'),
                 $ul = $('<ul/>'),
@@ -102,10 +105,12 @@
                 ulWidth = ( 100 + 5 ) * len; // #FIXME: magic number
 
             $navi[0].className = 'iv-navi';
+            $naviWrapper[0].className = 'iv-navi-wrapper';
             $naviPrev[0].className = 'iv-thumb-indicator prev';
             $naviNext[0].className = 'iv-thumb-indicator next';
             $ul[0].className = 'iv-thumb-list';
-            $navi.width(width);
+            $naviWrapper.width(width);
+            $navi.width(width + 2 * 40); // #FIXME: magic number
             // #TODO: 将css中li的宽度和js中li的宽度统一
             $ul.width(ulWidth);
 
@@ -123,10 +128,14 @@
 
             });
 
+            $navi.append($naviWrapper.append($ul));
+            $navi.append($naviPrev.append($('<span/>'))).append($naviNext.append($('<span/>')));
+
             // 事件绑定
             $ul.find('.iv-thumb-item').on('mouseover', function (event) {
 
-                var $t = $(this);
+                var $t = $(this),
+                    index = $t.index();
 
                 $t.find('img').is(':animated') && $t.find('img').dequeue();
                 $t.not('.selected').find('img').fadeTo(fadeTime, 1);
@@ -136,6 +145,7 @@
 
                 var $t = $(this);
 
+                event.stopPropagation(); // 阻止事件冒泡到外层ul上
                 $t.find('img').is(':animated') && $t.find('img').dequeue();
                 $t.not('.selected').find('img').fadeTo(fadeTime, opacity);
                 $t.removeClass('hover');
@@ -150,7 +160,73 @@
 
             });
 
-            $navi.append($ul);
+            $ul.on('mouseout', function (event) {
+
+                var $t = $(this),
+                    index = _t.$ul.find('.selected').index();
+
+                _t.transformImage(index);
+
+            });
+
+            var isHovered = false,
+                slideTimer,
+                enableHoverSlide = _t.transitionSetting.enableHoverSlide;
+
+            $navi.find('.prev,.next').on('mouseover', function (event) {
+
+                var $t = $(this),
+                    isForword;
+                
+                $t.addClass('hover');
+
+                if (!enableHoverSlide) return false;
+                    
+                isHovered = true;
+                isForword = $t[0].className.match('prev') ? false : true;
+                slideTimer = setInterval(function () {
+
+                    var marginLeft = $t.css('margin-left');
+
+                    if(isHovered === false) {
+                        clearInterval(slideTimer);
+                    } else if (!_t.$ul.is(':animated')){
+                        var offset = _t.thumbOffset + (isForword ? 100 : -100);
+                        _t.scrollThumb(offset, 'linear');
+                    }
+                    
+                } ,20);
+
+            }).on('mouseout', function (event) {
+
+                var $t = $(this);
+
+                $t.removeClass('hover');
+
+                if (!enableHoverSlide) return false;
+
+                if(_t.$ul.is(':animated') && enableHoverSlide) {
+                    _t.$ul.dequeue();
+                }
+
+                isHovered = false;
+                clearInterval(slideTimer);
+
+            });
+
+            $navi.find('.prev,.next').on('click', function (event) {
+
+                var $t = $(this),
+                    isForword = $t[0].className.match('prev') ? false : true,
+                    index = _t.currentIndex + (isForword ? _t.thumbNumber : (-_t.thumbNumber));
+
+                index = index < 0 ? 0 : (index > _t.maxIndex ? _t.maxIndex : index);
+
+                _t.setIndex(index);
+
+            });
+
+            
             _t.$navi = $navi;
             _t.$ul = $ul;
             _t.list = list;
@@ -183,24 +259,35 @@
         centerSelectedThumb: function (index) {
 
             var _t = this,
+                $naviWraper = _t.$navi.find('.iv-navi-wrapper'),
                 offsetLeft,
-                offsetRight,
                 scrollLeft,
-                scrollRight,
-                marginLeft,
-                maxMarginLeft;
+                marginLeft;
 
             offsetLeft = (100 + 5) * index; // #FIXME: magic number
-            scrollLeft = (_t.$navi.width() - 100) / 2;
-            maxMarginLeft = _t.$ul.width() - _t.$navi.width();
+            scrollLeft = ($naviWraper.width() - 100) / 2;
             marginLeft = offsetLeft - scrollLeft;
-            marginLeft =  marginLeft < 0 ? 0 : (marginLeft > maxMarginLeft ? maxMarginLeft : marginLeft);
+            _t.scrollThumb(marginLeft);
 
-            _t.$ul.is(':animated') && _t.$ul.stop();
-            _t.$ul.animate({'margin-left': '-' + marginLeft + 'px'}, 500);
         },
 
-        scrollThumb: function (offset) {
+        scrollThumb: function (offset, easing) {
+
+            var _t = this,
+                $naviWraper = _t.$navi.find('.iv-navi-wrapper'),
+                slideSpeed = _t.transitionSetting.slideSpeed,
+                maxMarginLeft = _t.$ul.width() - $naviWraper.width(),
+                slideTime = Math.abs(offset - _t.thumbOffset) / slideSpeed * 1000;
+
+            if(typeof arguments[1] === 'undefined') {
+                easing = _t.transitionSetting.thumbEasing;
+            }
+
+            offset =  offset < 0 ? 0 : (offset > maxMarginLeft ? maxMarginLeft : offset);
+
+            _t.$ul.is(':animated') && _t.$ul.stop();
+            _t.thumbOffset = offset;
+            _t.$ul.animate({'margin-left': '-' + offset + 'px'}, slideTime, easing);
 
         },
 
@@ -277,21 +364,27 @@
 
     $.fn.imageViewer.defaults = {
 
+        /* width, height, thumbHeight, thumbWidth变更时需重新编译scss文件，或手动指定样式 */
         width: 600,
         height: 500,
         thumbHeight: 80,
         thumbWidth: 100,
         currentIndex: 0,
-        wrapper: false, // jQuery object or false, if set to false, the wrapper is body instead
+        wrapper: false, // 包裹控件的jQuery对象选择器，设置为false时为body
         position: 'fixed', // 'fixed'|'absolute'|'normal'
         mask: true, // true|false
         loop: true, // true|false
         transitionSetting: {
+
+            enableHoverSwicth: true, // true|false 是否在鼠标悬停时切换图片（悬停结束后切换回原图片）
+            enableHoverSlide: false, // true|false 是否在“前进”、“后退”按钮悬停时滚动图片
+            slideSpeed: 2000, // 滚动速度：pixels/s
             opacity: 0.8,
             fadeTime: 100, // ms
-            thumbOpacity: 0.6,
-            thumbFadeTime: 200 // ms
-        } 
+            thumbOpacity: 0.5,
+            thumbFadeTime: 200, // ms
+            thumbEasing: 'linear'
+        }
     };
 
 })(jQuery);
