e19416cee45329cb8062e4f8e5492db4ea5725a5
icefox <icefox0801@hotmail.com>
2014-04-04 18:53:45 +0800

    combine with enhanced single image function

--- public/javascripts/imageViewerPlus.js
+++ public/javascripts/imageViewerPlus.js
@@ -3,10 +3,20 @@
  *
  **/
 (function($){
+
+    var isIE = !+[1,],
+        dequeue = function ($elem) {
+
+            if($elem.is(':animated')) $elem.dequeue();
+
+        };
     /* ImageViewer类定义，ImageViewer类封装并暴露了ImageSlider和ImageTool类的方法 */
     var ImageViewer = function (elem, album, options) {
 
         var imageSlider = new ImageSlider(elem, album, options);
+            wrapper = imageSlider.$container.get(0);
+            img = imageSlider.$img.get(0);
+            imageTool = new ImageTool(wrapper, img, options);
 
         return this;
     };
@@ -20,6 +30,7 @@
         this.album = album;
         this.$elem = $(elem);
         this.$wrapper = options.wrapper === false ? $('body') : $(options.wrapper);
+        this.$container = null;
         this.$img = null;
         this.$navi = null;
         this.$ul = null;
@@ -41,26 +52,27 @@
                 height = options.height;
 
             _t.$elem.hide();
-            _t.$elem.addClass('iv-container');
-            _t.$elem.append(_t.createWrapper(width, height));
+            _t.$elem.addClass('iv-wrapper');
+            _t.$elem.append(_t.createContainer(width, height));
             _t.$elem.append(_t.createNavi(_t.album, width));
             _t.setIndex(_t.currentIndex);
             _t.$elem.show();
         },
 
-        createWrapper: function (width, height) {
+        createContainer: function (width, height) {
 
             var _t = this,
-                $wrapper = $('<div/>'),
+                $container = $('<div/>'),
                 $next = $('<div/>').append('<span />'),
                 $prev = $('<div/>').append('<span />'),
                 $img = $('<img />'),
                 $content = $('<div/>').append($img);
 
-            $wrapper[0].className = 'iv-wrapper';
-            $next[0].className = 'iv-indicator next';
-            $prev[0].className = 'iv-indicator prev';
-            $content[0].className = 'iv-content';
+            $container.addClass('iv-container');
+            $next.addClass('iv-indicator next').attr('title', '下一张');
+            $prev.addClass('iv-indicator prev').attr('title', '上一张');
+            $container.width(width + 40 * 2); // #FIXME: magic number
+            $content.addClass('iv-content');
             $content.css({
                 width: width,
                 height: height
@@ -70,7 +82,7 @@
                 'max-height': height
             });
 
-            $wrapper.append($content).append($prev).append($next);
+            $container.append($content).append($prev).append($next);
 
             // 事件绑定
             $prev.on('click', function (event) {
@@ -90,9 +102,9 @@
             });
 
             _t.$img = $img;
-            _t.$wrapper = $wrapper;
+            _t.$container = $container;
 
-            return $wrapper;
+            return $container;
 
         },
 
@@ -111,13 +123,13 @@
                 len = album.length,
                 ulWidth = ( 100 + 5 ) * len; // #FIXME: magic number
 
-            $navi[0].className = 'iv-navi';
-            $naviWrapper[0].className = 'iv-navi-wrapper';
-            $naviPrev[0].className = 'iv-thumb-indicator prev';
-            $naviNext[0].className = 'iv-thumb-indicator next';
-            $ul[0].className = 'iv-thumb-list';
+            $navi.addClass('iv-navi');
+            $naviWrapper.addClass('iv-navi-wrapper');
+            $naviPrev.addClass('iv-thumb-indicator prev');
+            $naviNext.addClass('iv-thumb-indicator next');
+            $ul.addClass('iv-thumb-list');
             $naviWrapper.width(width);
-            $navi.width(width + 2 * 40); // #FIXME: magic number
+            $navi.width(width + 40 * 2); // #FIXME: magic number
             // #TODO: 将css中li的宽度和js中li的宽度统一
             $ul.width(ulWidth);
 
@@ -125,7 +137,7 @@
 
                 thumbImg[idx] = $('<img/>');
                 list[idx] = $('<li/>');
-                list[idx][0].className = 'iv-thumb-item';
+                list[idx].addClass('iv-thumb-item');
 
                 thumbImg[idx][0].src = album[idx];
                 thumbImg[idx].css({
@@ -144,19 +156,23 @@
                 var $t = $(this),
                     index = $t.index();
 
-                $t.find('img').is(':animated') && $t.find('img').dequeue();
-                $t.not('.selected').find('img').fadeTo(fadeTime, 1);
+                dequeue($t.find('img'));
+                dequeue(_t.$img);
+                $t.not('.selected').find('img').fadeTo(fadeTime, 1, function () {
+                    _t.transformImage(index);
+                });
                 $t.addClass('hover');
-
-                _t.transformImage(index);
 
             }).on('mouseout', function (event) {
 
-                var $t = $(this);
+                var $t = $(this),
+                    index = _t.$ul.find('.selected').index();
 
                 event.stopPropagation(); // 阻止事件冒泡到外层ul上
-                $t.find('img').is(':animated') && $t.find('img').dequeue();
-                $t.not('.selected').find('img').fadeTo(fadeTime, opacity);
+                dequeue(_t.$img);
+                $t.not('.selected').find('img').fadeTo(fadeTime, opacity, function () {
+                    _t.transformImage(index);
+                });
                 $t.removeClass('hover');
 
             }).on('click', function (event) {
@@ -164,21 +180,10 @@
                 var $t = $(this),
                     idx = $t.index();
 
-                $t.find('img').is(':animated') && $t.find('img').dequeue();
+                dequeue($t.find('img'));
                 _t.setIndex(idx);
 
             });
-
-            /* $ul.on('mouseout', function (event) {
-
-                var $t = $(this),
-                    index = _t.$ul.find('.selected').index();
-
-                if(this !== event.target) return false;
-
-                _t.transformImage(index);
-
-            }); */
 
             var isHovered = false,
                 slideTimer,
@@ -216,10 +221,7 @@
 
                 if (!enableHoverSlide) return false;
 
-                if(_t.$ul.is(':animated') && enableHoverSlide) {
-                    _t.$ul.dequeue();
-                }
-
+                dequeue(_t.$ul);
                 isHovered = false;
                 clearInterval(slideTimer);
 
@@ -296,7 +298,7 @@
 
             offset =  offset < 0 ? 0 : (offset > maxMarginLeft ? maxMarginLeft : offset);
 
-            _t.$ul.is(':animated') && _t.$ul.stop();
+            if(_t.$ul.is(':animated')) _t.$ul.stop();
             _t.thumbOffset = offset;
             _t.$ul.animate({'margin-left': '-' + offset + 'px'}, slideTime, easing);
 
@@ -308,7 +310,7 @@
                 fadeTime = _t.transitionSetting.fadeTime,
                 opacity = _t.transitionSetting.opacity;
 
-            _t.$img.is(':animated') && _t.$img.dequeue();
+            dequeue(_t.$img);
             _t.$img.fadeTo(fadeTime, opacity, function() {
                 _t.$img[0].src = _t.album[index];
             }).fadeTo(fadeTime, 1);
@@ -328,16 +330,20 @@
 
         destroy: function () {
 
-        },
-
-        switchImage: function (index) {
-
         }
 
     };
 
     /* ImageUtil Class Definition*/
-    var ImageTool = function(img) {
+    var ImageTool = function(wrapper, img, options) {
+
+        if(!options.enableToolbar) return false;
+
+        this.$wrapper = $(wrapper);
+        this.$img = $(img);
+        this.$toolbar = null;
+        this.rotate = 0;
+        this.init(options);
 
     };
 
@@ -345,9 +351,195 @@
 
         constructor: ImageTool,
 
-        getToolbar: function () {
-            return;
+        init: function (options) {
+
+            var _t = this,
+                img = _t.$img.get(0);
+
+            _t.$img.addClass('iv-image-enhanced');
+            _t.$wrapper.append(_t.createToolbar());
+            _t.bindKeydownEvents();
+            _t.imgReady(img);
+
+        },
+
+        createToolbar: function () {
+
+            var _t = this,
+                $toolbar = $('<div/>'),
+                $toolbarCon = $('<div/>'),
+                $rotateLeftBtn = $('<a href="javascript:void(0)"/>'),
+                $rotateRightBtn = $('<a href="javascript:void(0)"/>'),
+                $zoomInBtn = $('<a href="javascript:void(0)"/>'),
+                $zoomOutBtn = $('<a href="javascript:void(0)"/>'),
+                $closeBtn = $('<a href="javascript:void(0)"/>');
+
+            $toolbar.addClass('iv-toolbar');
+            $toolbarCon.addClass('iv-toolbar-con');
+            $rotateLeftBtn.addClass('iv-rotate-left-btn').attr("title","左转");
+            $rotateRightBtn.addClass('iv-rotate-right-btn').attr("title","右转");
+            $zoomInBtn.addClass('iv-zoom-in-btn').attr("title","原始大小");
+            $zoomOutBtn.addClass('iv-zoom-out-btn').attr("title","适应窗口");
+            $closeBtn.addClass('iv-close-btn').attr("title","关闭");
+
+            // 按钮事件
+            $rotateLeftBtn.on('click', function (event) {
+                _t.rotateLeft();
+            });
+
+            $rotateRightBtn.on('click', function (event) {
+                _t.rotateRight();
+            });
+
+            $zoomInBtn.on('click', _t.zoomIn);
+            $zoomOutBtn.on('click', _t.zoomOut);
+            $closeBtn.on('click', _t.closeDialog);
+            
+            $toolbarCon.append($rotateLeftBtn).append($rotateRightBtn);
+            $toolbarCon.append($zoomInBtn).append($zoomOutBtn);
+            $toolbarCon.append($closeBtn);
+            $toolbar.append($toolbarCon);
+
+            _t.$toolbar = $toolbar;
+            return $toolbar;
+        },
+
+        imgReady: function (img) {
+
+            img.onload = function(){
+
+                imgObjWidth = img.width;
+                imgObjHeight = img.height;
+                dragMaxLeft = imgObjWidth - width;
+                dragMaxTop = imgObjHeight - height;
+                dragGap = (imgObjWidth-imgObjHeight)/2;
+                if(width/height > imgObjWidth/imgObjHeight){
+                    imgZoomWidth = parseInt(imgObjWidth/imgObjHeight*height);
+                    imgZoomHeight = height;
+                }else{
+                    imgZoomWidth = width;
+                    imgZoomHeight = parseInt(imgObjHeight/imgObjWidth*width);
+                }
+
+                imgObj
+                    .attr("width",imgZoomWidth).attr("height",imgZoomHeight)
+                    .css({
+                        left:(width-imgZoomWidth)/2,
+                        top:(height-imgZoomHeight)/2,
+                        cursor:"move"
+                    });
+                dragTop = (height-imgZoomHeight)/2;
+                dragLeft = (width-imgZoomWidth)/2;  
+                if(isIE){
+                    imgObj.css("filter","progid:DXImageTransform.Microsoft.BasicImage(rotation=0)");
+                }else{
+                    imgObj
+                        .css("-webkit-transform","rotate(0deg)")
+                        .css("-moz-transform","rotate(0deg)")
+                        .css("-o-transform","rotate(0deg)")
+                        .css("transform","rotate(0deg)");
+                }
+            }
+            img.src=src;
+
+        },
+
+        bindKeydownEvents: function () {
+
+            var _t = this,
+                img = _t.$img.get(0);
+
+            $(document).on("keydown",function(event){
+
+                if(!!img){
+                    switch(event.keyCode){
+                        case 83://s
+                            _t.rotateLeft();
+                            break;
+                        case 70://f
+                            _t.rotateRight();
+                            break;
+                        case 68://d
+                            if(zoomIn){
+                                _t.zoomOut();
+                            }else{
+                                _t.zoomIn();
+                            }
+                            break;
+                        case 67://c
+                            _t.closeDialog();
+                            break;
+                    }
+                }
+
+            });
+        },
+
+        setRotateAnim: function () {
+
+            var _t = this,
+                $img = _t.$img;
+
+            if(isIE){
+                $img.css('filter','progid:DXImageTransform.Microsoft.BasicImage(rotation='+(rotate<0?4-(-rotate%4):rotate%4)+')');
+                
+                if(!zoomIn){
+
+                    if(_t.rotate%2 == 1){
+                        $img.css({
+                            left: (width - imgZoomHeight) / 2,
+                            top: (height - imgZoomWidth) / 2
+                        });
+                    } else {
+                        $img.css({
+                            left: (width - imgZoomWidth) / 2,
+                            top: (height - imgZoomHeight) / 2
+                        });
+                    }
+
+                }
+
+            } else {
+                $img.css('-webkit-transform', 'rotate('+_t.rotate*90+'deg)')
+                    .css('-moz-transform', 'rotate('+_t.rotate*90+'deg)')
+                    .css('-o-transform', 'rotate('+_t.rotate*90+'deg)')
+                    .css('transform', 'rotate('+_t.rotate*90+'deg)');
+            }
+
+        },
+
+        rotateLeft: function () {
+
+            var _t = this;
+
+            _t.rotate--;
+            _t.setRotateAnim();
+
+            return false;
+        },
+
+        rotateRight: function () {
+
+            var _t = this;
+
+            _t.rotate++;
+            _t.setRotateAnim();
+
+            return false;
+        },
+
+        zoomIn: function () {
+
+        },
+
+        zoomOut: function () {
+
+        },
+
+        closeDialog: function () {
+
         }
+
     };
 
     /* Integration with jQuery*/
@@ -379,6 +571,7 @@
 
     $.fn.imageViewer.defaults = {
 
+        enableToolbar: true,
         /* width, height, thumbHeight, thumbWidth变更时需重新编译scss文件，或手动指定样式 */
         width: 600,
         height: 500,
