791aa1511d10b3cf92ba059080c71e9261a21121
icefox <icefox0801@hotmail.com>
2014-04-08 11:12:38 +0800

    Adding comments to generate docs

--- public/javascripts/imageViewerPlus.js
+++ public/javascripts/imageViewerPlus.js
@@ -25,22 +25,23 @@
 
         };
     /**
-<<<<<<< HEAD
      * @name ImageViewer
      * @class ImageViewer类定义，ImageViewer类封装并暴露了ImageSlider和ImageTool类的方法
      * @constructor
-     * @param {object}  elem        imageViewer元素
-     * @param {array}   album       图片数组
-     * @param {object}  options     控件选项
-     * @param {boolean} options.enableToolbar   是否启用图片工具条
-     * @param {number}  options.width           图片宽度
-     * @param {number}  options.height          图片高度
-     * @param {number}  options.thumbHeight     图片缩略图高度
-     * @param {number}  options.thumbWidth      图片缩略图宽度
-     * @param {number}  options.currentIndex    要显示的图片索引
-     * @param {string}  options.wrapper         包裹控件的jQuery对象选择器，设置为false时为body
-     * @param {string}  options.position        图片控件定位，'fixed'|'absolute'|'normal'
-     * @param {string}  options.showMask        是否显示图片遮罩层
+     * @param {object} elem    imageViewer元素
+     * @param {array} album   图片数组
+     * @param {object} options 控件选项
+     * @param {number} options.width 图片宽度
+     * @param {boolean} enableToolbar 是否启用图片工具条
+     * : true,
+        /* width, height, thumbHeight, thumbWidth变更时需重新编译scss文件，或手动指定样式 */
+        width: 600,
+        height: 500,
+        thumbHeight: 80,
+        thumbWidth: 100,
+        currentIndex: 0, // 当前图片索引
+        wrapper: false, // 包裹控件的jQuery对象选择器，设置为false时为body
+        position: 'fixed', // 'fixed'|'absolute'|'normal'
         mask: true, // true|false
         loop: true, // true|false
         transitionSetting: {
@@ -55,12 +56,6 @@
             thumbFadeTime: 100, // ms
             thumbEasing: 'linear'
         }
-=======
-     * ImageViewer类定义，ImageViewer类封装并暴露了ImageSlider和ImageTool类的方法
-     * @param {[type]} elem    imageViewer元素
-     * @param {[type]} album   图片数组
-     * @param {[type]} options 控件选项
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
      */
     var ImageViewer = function (elem, album, options) {
         var imageSlider = new ImageSlider(elem, album, options);
@@ -69,15 +64,11 @@
             imageTool = new ImageTool(wrapper, img, options);
 
         optionsCache = options;
-<<<<<<< HEAD
         /**
          * 重构方法
          * @name ImageViewer.refactor
          * @return {ImageViewer} 重构后的Imagerviewr实例
          */
-=======
-        // 重构方法
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
         this.refactor = function () {
             imageSlider.refactor.apply(imageSlider, arguments); // 重构方法
             return this;
@@ -105,18 +96,11 @@
         };
     };
     /**
-<<<<<<< HEAD
      * @class  ImageSlider
      * @description ImageSlider类定义
      * @param {object} elem    imageSlider元素
      * @param {array} album   图片数组
      * @param {object} options 图片控件选项
-=======
-     * ImageSlider类定义
-     * @param {[type]} elem    imageSlider元素
-     * @param {[type]} album   图片数组
-     * @param {[type]} options 图片控件选项
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
      */
     var ImageSlider = function (elem, album, options) {
         this.album = album; // 图片数组
@@ -140,7 +124,6 @@
     ImageSlider.prototype = {
         /**
          * ImageSlider构造方法
-<<<<<<< HEAD
          * 
          */
         constructor: ImageSlider,
@@ -148,14 +131,6 @@
          * 初始化方法
          * @lends ImageSlider.prototype
          * @param  {object} options 图片控件选项
-=======
-         * @type {[type]}
-         */
-        constructor: ImageSlider,
-        /**
-         * ImageSlider初始化方法
-         * @param  {[type]} options 图片控件选项
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         init: function (options) {
             var _t = this,
@@ -175,12 +150,8 @@
         },
         /**
          * 初始化对象参数
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {object} options 图片控件选项
-=======
-         * @param  {[type]} options 图片控件选项
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         initParam: function (options) {
             var _t = this;
@@ -196,16 +167,10 @@
         },
         /**
          * 重构图片控件对象
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {array} album   图片数组
          * @param  {object} options 图片控件选项
          * @return {object}         imageSlider对象
-=======
-         * @param  {[type]} album   图片数组
-         * @param  {[type]} options 图片控件选项
-         * @return {[type]}         imageSlider对象
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         refactor: function (album, options) {
             var _t = this,
@@ -240,11 +205,7 @@
         },
         /**
          * 销毁imageSlider对象
-<<<<<<< HEAD
          * @return {undefined} 销毁后imageSlider对象--undefined
-=======
-         * @return {[type]} 销毁后imageSlider对象--undefined
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         destroy: function () {
             this.$elem.empty();
@@ -254,16 +215,10 @@
         },
         /**
          * 创建图片预览区
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {number} width  图片外层DIV宽度（图片最大宽度）
          * @param  {number} height 图片外层DIV高度（图片最大高度）
          * @return {object}        图片预览区jQuery元素
-=======
-         * @param  {[type]} width  图片外层DIV宽度（图片最大宽度）
-         * @param  {[type]} height 图片外层DIV高度（图片最大高度）
-         * @return {[type]}        图片预览区jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         createContainer: function (width, height) {
             var _t = this,
@@ -300,11 +255,7 @@
         },
         /**
          * 创建遮罩
-<<<<<<< HEAD
          * @return {object} 遮罩层jQuery元素
-=======
-         * @return {[type]} 遮罩层jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         createMask: function () {
             var _t = this,
@@ -316,14 +267,9 @@
         },
         /**
          * 翻页事件绑定
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {object} $prev 上一张按钮
          * @param  {object} $next 下一张按钮
-=======
-         * @param  {[type]} $prev 上一张按钮
-         * @param  {[type]} $next 下一张按钮
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         bindPagingEvents: function ($prev, $next) {
             var _t = this;
@@ -346,16 +292,10 @@
         },
         /**
          * 创建图片导航区
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {array} album 图片数组
          * @param  {number} width 图片外层DIV宽度
          * @return {object}       图片导航区jQuery元素
-=======
-         * @param  {[type]} album 图片导航
-         * @param  {[type]} width 图片外层DIV宽度
-         * @return {[type]}       图片导航区jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         createNavi: function (album, width, thumbWidth) {
             var _t = this,
@@ -389,14 +329,9 @@
         },
         /**
          * 创建缩略图列表
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {array} album 图片数组
          * @return {object}       缩略图列表jQuery元素
-=======
-         * @param  {[type]} album 图片数组
-         * @return {[type]}       缩略图列表jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         createThumbList: function (album) {
             var _t = this,
@@ -416,14 +351,9 @@
         },
         /**
          * 创建单个缩略图及外层LI
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {string} src 图片src地址
          * @return {object}     HTMLLIElement
-=======
-         * @param  {[type]} src 图片src地址
-         * @return {[type]}     HTMLLIElement
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         createThumbItem: function (src) {
             var _t = this,
@@ -442,12 +372,8 @@
         },
         /**
          * 缩略图事件绑定
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {object} $ul 缩略图列表jQuery元素
-=======
-         * @param  {[type]} $ul 缩略图列表jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         bindThumbEvents: function ($ul) {
             var _t = this,
@@ -519,12 +445,8 @@
         },
         /**
          * 图片导航区翻页事件绑定
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {object} $navi 图片导航jQuery元素
-=======
-         * @param  {[type]} $navi 图片导航jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         bindThumbPagingEvents: function ($navi) {
             var _t = this,
@@ -575,14 +497,9 @@
         },
         /**
          * 滑动缩略图列表
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {boolean} isForword 是否为前进方向:true为向前
          * @return {object}            setInterval计时器
-=======
-         * @param  {Boolean} isForword 是否为前进方向:true为向前
-         * @return {[type]}            setInterval计时器
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         slideThumb: function (isForword) {
             var _t = this,
@@ -614,12 +531,8 @@
         },
         /**
          * 设置图片
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param {number} index 图片的索引
-=======
-         * @param {[type]} index 图片的索引
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         setIndex: function (index) {
             var _t = this;
@@ -633,12 +546,8 @@
         },
         /**
          * 预览图片
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {number} index 图片的索引
-=======
-         * @param  {[type]} index 图片的索引
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         previewIndex: function (index) {
             var _t = this;
@@ -652,12 +561,8 @@
         },
         /**
          * 高亮缩略图并添加相应CLASS NAME
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {number} index 图片的索引
-=======
-         * @param  {[type]} index 图片的索引
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         highlightThumb: function (index, className) {
             var _t = this,
@@ -674,12 +579,8 @@
         },
         /**
          * 将当前的缩略图居中
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {number} index 当前图片的索引
-=======
-         * @param  {[type]} index 当前图片的索引
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         centerThumb: function (index) {
             var _t = this,
@@ -696,14 +597,9 @@
         },
         /**
          * 滚动缩略图导航条
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {number} offset 滚动到的位移（缩略图列表的margin-left）
          * @param  {number} easing 动画类型
-=======
-         * @param  {[type]} offset 滚动到的位移（缩略图列表的margin-left）
-         * @param  {[type]} easing 动画类型
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         scrollThumb: function (offset, easing) {
             var _t = this,
@@ -723,12 +619,8 @@
         },
         /**
          * 切换图片
-<<<<<<< HEAD
          * @memberOf ImageSlider
          * @param  {number} index 图片的索引
-=======
-         * @param  {[type]} index 图片的索引
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         transformImage: function (index) {
             var _t = this,
@@ -759,10 +651,6 @@
         },
         /**
          * 绑定按键事件
-<<<<<<< HEAD
-=======
-         * @return {[type]} [description]
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         bindKeydownEvents: function () {
             var _t = this;
@@ -784,18 +672,11 @@
         }
     };
     /**
-<<<<<<< HEAD
      * @class ImageTool
      * @description 图片工具条类
      * @param {object} container 图片外层容器
      * @param {object} img       图片元素
      * @param {object} options   图片控件参数
-=======
-     * 图片工具条类
-     * @param {[type]} container 图片外层容器
-     * @param {[type]} img       图片元素
-     * @param {[type]} options   图片控件参数
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
      */
     var ImageTool = function(container, img, options) {
 
@@ -826,11 +707,7 @@
         constructor: ImageTool,
         /**
          * 初始化方法
-<<<<<<< HEAD
          * @param  {object} options 图片控件参数
-=======
-         * @param  {[type]} options 图片控件参数
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         init: function (options) {
 
@@ -851,7 +728,6 @@
         },
         /**
          * 创建拖拽遮罩
-<<<<<<< HEAD
          * @return {object} 拖拽遮罩jQuery元素
          */
         createDragMask: function () {
@@ -859,15 +735,6 @@
             var _t = this,
                 $dragMask = $('<div/>');
 
-=======
-         * @return {[type]} 拖拽遮罩jQuery元素
-         */
-        createDragMask: function () {
-
-            var _t = this,
-                $dragMask = $('<div/>');
-
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
             $dragMask.addClass('iv-drag-mask');
             $dragMask.hide();
 
@@ -875,11 +742,7 @@
         },
         /**
          * 创建图片工具条
-<<<<<<< HEAD
          * @return {object} 图片工具条jQuery元素
-=======
-         * @return {[type]} 图片工具条jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         createToolbar: function () {
 
@@ -970,7 +833,6 @@
         rotateLeft: function () {
 
             var _t = this;
-<<<<<<< HEAD
 
             _t.rotate--;
             _t.setRotateAnim();
@@ -1020,59 +882,6 @@
                 if(!_t.isDrag) return false;
 
                 if(event.target === img){
-=======
-
-            _t.rotate--;
-            _t.setRotateAnim();
-        },
-        /**
-         * 顺时针旋转
-         */
-        rotateRight: function () {
-
-            var _t = this;
-
-            _t.rotate++;
-            _t.setRotateAnim();
-        },
-        /**
-         * 图片拖拽事件绑定
-         */
-        bindDragEvents: function () {
-            var _t = this,
-                $img = _t.$img,
-                img = _t.$img.get(0),
-                dragX, // 拖拽的X坐标
-                dragY, // 拖拽的Y坐标
-                endDrag = function () { // 拖拽结束时的处理函数
-                    _t.isMoved = false;
-                    _t.isDrag = false;
-                    _t.dragLeft = parseInt($img.css("left"), 10);
-                    _t.dragTop = parseInt($img.css("top"), 10);
-                    _t.$dragMask.hide();
-                    $img.removeClass('iv-in-drag');
-                };
-            // 鼠标按下、鼠标移动、鼠标按键弹起时的事件
-            $img.on("mousedown",function(event){
-
-                if(event.target === img){
-                    _t.dragLeft = parseInt($img.css('left'), 10);
-                    _t.dragTop = parseInt($img.css('top'), 10);
-                    dragX = event.pageX - _t.dragLeft; // 拖拽坐标起始值
-                    dragY = event.pageY - _t.dragTop; // 拖拽坐标起始值
-                    _t.isDrag = true;
-                }
-
-                return false;
-            }).on("mousemove",function(event){
-
-
-                if(!_t.isDrag) return false;
-
-                if(event.target === img){
-
-                    console.log(_t.dragLeft + '  ' + _t.dragTop);
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
                     var _x = event.pageX - dragX, // 拖拽的相对坐标
                         _y = event.pageY - dragY; // 拖拽的相对坐标
 
@@ -1138,7 +947,6 @@
 
             _t.dragLeft = left - _left;
             _t.dragTop = top - _top;
-<<<<<<< HEAD
 
             _t.isZoom = true;
         },
@@ -1211,89 +1019,13 @@
                             break;
                     }
 
-=======
-
-            _t.isZoom = true;
-        },
-        /**
-         * 缩小图片
-         */
-        zoomOut: function () {
-            var _t = this,
-                img = _t.$img.get(0),
-                left = parseInt(img.style.left, 10),
-                top = parseInt(img.style.top, 10),
-                _left,
-                _top;
-
-            if(!_t.isZoom) return false;
-            
-            _t.zoomImgWidth = _t.$img.width();
-            _t.zoomImgHeight = _t.$img.height();
-            _t.$img.removeClass('iv-zoom-in');
-            _t.imgWidth = _t.$img.width();
-            _t.imgHeight = _t.$img.height();
-
-            _left = (_t.zoomImgWidth - _t.imgWidth) / 2;
-            _top = (_t.zoomImgHeight - _t.imgHeight) / 2;
-
-            _t.$img.css({
-                left: left + _left + 'px',
-                top: top + _top + 'px'
-            });
-
-            _t.dragLeft = left + _left;
-            _t.dragTop = top + _top;
-
-            _t.isZoom = false;
-        },
-        /**
-         * 关闭窗口
-         */
-        closeDialog: function () {
-            var _t = this;
-            _t.$container.closest('.iv-wrapper').hide();
-        },
-        /**
-         * 键盘事件绑定
-         */
-        bindKeydownEvents: function () {
-            var _t = this,
-                img = _t.$img.get(0);
-
-            $(document).on("keydown",function(event){
-
-                if(!!img){
-                    switch(event.keyCode){
-                        case 83://S
-                            _t.rotateLeft();
-                            break;
-                        case 70://F
-                            _t.rotateRight();
-                            break;
-                        case 68://D
-                            if(_t.isZoom){
-                                _t.zoomOut();
-                            }else{
-                                _t.zoomIn();
-                            }
-                            break;
-                        case 67://C
-                            _t.closeDialog();
-                            break;
-                    }
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
                 }
 
             });
         },
         /**
          * 图片加载完成的事件
-<<<<<<< HEAD
          * @param  {object} $img 图片jQuery元素
-=======
-         * @param  {[type]} $img 图片jQuery元素
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
          */
         imgReady: function ($img) {
             var _t = this;
@@ -1308,15 +1040,9 @@
     };
     /**
      * 集成到jQuery对象
-<<<<<<< HEAD
      * @param  {array} album   图片数组
      * @param  {object} options 图片控件选项
      * @return {array}         imageViewer实例对象数组
-=======
-     * @param  {[type]} album   图片数组
-     * @param  {[type]} options 图片控件选项
-     * @return {[type]}         imageViewer实例对象数组
->>>>>>> ea2bba7798cede77f93ee6f92df31622f117a867
      */
     $.fn.imageViewer = function (album, options) {
 
